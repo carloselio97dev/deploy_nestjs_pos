b5705265162e03dff488ea9f6c3bc4409a4163e9
Carlos Elio <carloseliogonzalezapa1997@gmail.com>
2025-06-22 21:28:17 -0500

    Finish

--- src/api.ts
+++ src/api.ts
@@ -1,6 +1,5 @@
 import { TransactionsResponseSchema } from "./schemas";
 
-
 export async function getSalesByDate(date: string) {
   const base = process.env.NEXT_PUBLIC_API_URL!
   const res = await fetch(`${base}/transactions?transactionDate=${date}`, {
