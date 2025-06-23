8cd44ff1c7b14c4e12d032b41ef0f9e7fea4bef8
Carlos Elio <carloseliogonzalezapa1997@gmail.com>
2025-06-22 21:18:28 -0500

    F

--- src/api.ts
+++ src/api.ts
@@ -1,20 +1,11 @@
 import { TransactionsResponseSchema } from "./schemas";
 
+
 export async function getSalesByDate(date: string) {
-    const url = `${process.env.NEXT_PUBLIC_DOMAIN}/admin/sales/api?transactionDate=${date}`;
-    const req = await fetch(url);
-
-    if (!req.ok) {
-        throw new Error("No se pudo obtener ventas del servidor");
-    }
-    try {
-        const json = await req.json();
-        const transactions = TransactionsResponseSchema.parse(json);
-        return transactions;
-    }
-    catch (error) {
-         console.error("Zod validation error", error);
-         throw new Error("Error al validar los datos de ventas");
-    }
+  const base = process.env.NEXT_PUBLIC_API_URL!
+  const res = await fetch(`${base}/transactions?transactionDate=${date}`, {
+    credentials: 'include',
+  })
+  if (!res.ok) throw new Error('No se pudo obtener ventas')
+  return TransactionsResponseSchema.parse(await res.json())
 }
-
